46f783f11006566b77eee57d3babe3964c01745d
Edward <edward.lee3755@gmail.com>
2018-04-21 02:31:32 -0600

    Using result['url'] to access cloudinary array result to get the url for the uploaded img.

--- app/routes/api.js
+++ app/routes/api.js
@@ -477,7 +477,8 @@ module.exports = function(router) { // need to export so that we can import into
       post.postImgUrl = req.body.postImgUrl;
 
       cloudinary.uploader.upload(req.body.postImg, function(result) { //req.body.postImg contains the full base64 encoded string which we can use to use cloudinary api for upload
-        post.postImgUrl = result.url;
+        var resultUrl = result['url'];
+        post.postImgUrl = resultUrl;
       });
     }
 
