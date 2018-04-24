8b918561f68f52a7c274eff924b9f0c81794d7c1
Edward <edward.lee3755@gmail.com>
2018-04-23 22:59:21 -0600

    Added variable for filetype as well as added ( ) to seperate or statement conditionals

--- app/routes/api.js
+++ app/routes/api.js
@@ -483,9 +483,10 @@ module.exports = function(router) { // need to export so that we can import into
       }); // post.save
     } else {  // else if image provided
       // check file size and file type
+      var fileType = req.body.contentType;
       if (req.body.postImg.size > 10 * 1024 * 1024) { // converting bytes to kb to mb
           res.json({ success: false, message: "File size is too large. Max limit is 10MB" });
-      } else if (req.body.contentType != "image/jpeg" || req.body.contentType != "image/jpg" || req.body.contentType != "image/png") {
+      } else if ( (fileType != "image/jpeg") || (fileType != "image/jpg") || (fileType != "image/png") ) {
           res.json({ success: false, message: "Filetype is invalid. Must be .png, .jpeg, jpg" });
       }
       else { // else if image size and contentType is valid
